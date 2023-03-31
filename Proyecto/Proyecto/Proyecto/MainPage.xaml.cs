c4ada040f6d6931aec6c83131110c69247c2436a
Isaias <19011808@itsoeh.edu.mx>
2023-03-31 01:21:32 -0600

    Proyecto FarmaMovil

---
+++ Proyecto/Proyecto/Proyecto/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Proyecto
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Menu(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View.Login());
+        }
+        private async void Registro(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View.Registro());
+        }
+    }
+}
